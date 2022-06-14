85cc81d2470a13d2ff251615750198f31b434388
zhanghaojie <1455123087@qq.com>
2022-06-14 14:56:55 +0800

    feat: add useObserveSelection hook

---
+++ src/components/useObserveSelection.ts
@@ -0,0 +1,27 @@
+import { useEffect, useRef, useState } from 'react';
+import { SelectionObserver, useSelectionContext } from './context';
+
+function useObserveSelection<T>(observer: SelectionObserver<T>) {
+  const { observe } = useSelectionContext();
+  const [intersecting, setIntersecting] = useState(false);
+  const observerRef = useRef(observer);
+  useEffect(() => {
+    observerRef.current = observer;
+  });
+
+  useEffect(() => {
+    const unobserve = observe((selection) => {
+      const observeResult = observerRef.current(selection);
+      setIntersecting(observeResult.intersecting);
+      return observeResult;
+    });
+
+    return () => {
+      unobserve();
+    };
+  }, []);
+
+  return [intersecting] as [typeof intersecting];
+}
+
+export default useObserveSelection;
